f4b6987bccb12313436d84ca105f5f994ca21ac6
Elvis Dedic <elvis26112009@gmail.com>
2018-05-21 16:07:38 +0200

    use p2p server

--- umarchain/app/server.js
+++ umarchain/app/server.js
@@ -3,11 +3,13 @@ import bodyParser from 'body-parser'
 
 import Blockchain from '../lib/blockchain'
 import { HTTP_PORT } from '../utils/constants'
+import P2pServer from './p2p-server'
 
 const app = express()
 app.use(bodyParser.json())
 
 const bc = new Blockchain()
+const p2pServer = new P2pServer(bc)
 
 app.get('/blocks', (req, res) => {
   res.json(bc.chain)
@@ -16,8 +18,11 @@ app.get('/blocks', (req, res) => {
 app.post('/mine', (req, res) => {
   const block = bc.addBlock(req.body.data)
   res.redirect('/blocks')
+
+  p2pServer.syncChains()
 })
 
 app.listen(HTTP_PORT, () => {
   console.log('running like a hurrican', HTTP_PORT)
 })
+p2pServer.listen()
