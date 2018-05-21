af5f39597104e31c09fbbd0f56e2e15832ac1bca
Elvis Dedic <elvis26112009@gmail.com>
2018-05-21 16:07:55 +0200

    more constants

--- umarchain/utils/constants.js
+++ umarchain/utils/constants.js
@@ -1,7 +1,11 @@
 const GENESIS_TIME = 'Genesis time'
 const HTTP_PORT = process.env.HTTP_PORT || 3000
+const P2P_PORT = process.env.P2P_PORT || 5001
+const PEERS = process.env.PEERS ? process.env.PEERS.split(',') : []
 
 export {
   GENESIS_TIME,
-  HTTP_PORT
+  HTTP_PORT,
+  P2P_PORT,
+  PEERS
 }
