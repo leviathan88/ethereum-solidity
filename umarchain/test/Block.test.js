12be365c0745229e7766b1b69b39e8c4e51a9b45
Elvis Dedic <elvis26112009@gmail.com>
2018-05-21 12:42:51 +0200

    Block Tests

--- umarchain/test/Block.test.js
+++ umarchain/test/Block.test.js
@@ -1,18 +1,26 @@
-import Block from '../block'
+import Block from '../lib/Block'
 import assert from 'assert'
+import { GENESIS_TIME } from '../utils/constants';
 
-let block
+describe('Block Tests', () => {
 
-beforeEach(() => {
-  block = new Block('foo', 'bar', 'zoo', 'bazooka')
-})
+  let block
 
-describe('Blockchain', () => {
-  it('should check for creation of blockchain', () => {
+  beforeEach(() => {
+    block = new Block('foo', 'bar', 'zoo', 'bazooka')
+  })
+  
+
+  it('should check for creation of new Block', () => {
     assert.equal(block.timestamp, 'foo')
   })
 
-  it('shoud get genesis block', () => {
-    assert.equal(Block.genesis().timestamp, 'Genesis time')
+  it('shoud get Genesis Block', () => {
+    assert.equal(Block.genesis().timestamp, GENESIS_TIME)
+  })
+
+  it('should mine new Block', () => {
+    const newBlock = Block.mineBlock(block, 'Super Data!')
+    assert.equal(newBlock.data, 'Super Data!')
   })
 })
