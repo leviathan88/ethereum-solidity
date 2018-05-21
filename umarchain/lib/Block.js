fa15bd9ffa5cb383327380a3eaacd9142179480c
Elvis Dedic <elvis26112009@gmail.com>
2018-05-21 13:10:31 +0200

    implemebnt blockHash

--- umarchain/lib/Block.js
+++ umarchain/lib/Block.js
@@ -37,6 +37,11 @@ class Block {
     return SHA256(`${timestamp}${lastHash}${data}`).toString()
   }
   
+  static blockHash(block) {
+    const { timestamp, lastHash, data } = block
+
+    return Block.hash(timestamp, lastHash, data)
+  }
 }
 
 export default Block
