1f5c9cb5ca4da5c0277d0da71d53e38e227bdee6
Elvis Dedic <elvis26112009@gmail.com>
2018-05-31 16:12:14 +0200

    mine rate and difficulty

--- umarchain/utils/constants.js
+++ umarchain/utils/constants.js
@@ -1,13 +1,15 @@
-const GENESIS_TIME = 'Genesis time'
+const GENESIS_TIME = Date.now()
 const HTTP_PORT = process.env.HTTP_PORT || 3000
 const P2P_PORT = process.env.P2P_PORT || 5001
 const PEERS = process.env.PEERS ? process.env.PEERS.split(',') : []
-const DIFFICULTY = 4
+const DIFFICULTY = 3
+const MINE_RATE = 3000
 
 export {
   GENESIS_TIME,
   HTTP_PORT,
   P2P_PORT,
   PEERS,
-  DIFFICULTY
+  DIFFICULTY,
+  MINE_RATE
 }
