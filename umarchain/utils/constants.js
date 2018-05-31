9718e180880e4c4128c12dfafa83d454980cc3b3
Elvis Dedic <elvis26112009@gmail.com>
2018-05-31 13:47:28 +0200

    add diff

--- umarchain/utils/constants.js
+++ umarchain/utils/constants.js
@@ -2,10 +2,12 @@ const GENESIS_TIME = 'Genesis time'
 const HTTP_PORT = process.env.HTTP_PORT || 3000
 const P2P_PORT = process.env.P2P_PORT || 5001
 const PEERS = process.env.PEERS ? process.env.PEERS.split(',') : []
+const DIFFICULTY = 4
 
 export {
   GENESIS_TIME,
   HTTP_PORT,
   P2P_PORT,
-  PEERS
+  PEERS,
+  DIFFICULTY
 }
